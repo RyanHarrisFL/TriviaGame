23d2eb4eb6968325a8fb6d7bce4d82ed7d3177e0
Rhino <ryanharrisfl@gmail.com>
2019-03-07 16:15:42 -0500

    fixed issue with adding values to counters.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var countDown = 60;
+var countDown = 10;
 var correctAnswers = 0;
 var wrongAnswers = 0;
 var unAnswered = 0;
@@ -7,12 +7,21 @@ var intervalId;
 $("#start").on("click", startGame);
 $("#done").on("click", results);
 $("#countdown").html("<h1>" + countDown + "</h1>");
+$("#correct").html("<h1>" + correctAnswers + "</h1>");
+$("#wrong").html("<h1>" + wrongAnswers + "</h1>");
+$("#unanswered").html("<h1>" + unAnswered + "</h1>");
 
 $(document).ready(function(){
     $("#questions").hide();
-    $("#final-results");
+    $("#done").hide();
+    $("#correct").hide();
+    $("#wrong").hide();
+    $("#unAnswered").hide();
+    $("#final-results").hide();
     $("#start").click( function(){
         $("section").show();
+        $("#done").show();
+        $(this).hide();
     });
   });
 
@@ -33,6 +42,21 @@ if (countDown === 0) {
 
 function results() {
     clearInterval(intervalId);
-    console.log("You're a winner!")
+    $("#questions").hide();
+    $("#done").hide();
+    $("#correct").show();
+    $("#wrong").show();
+    $("#unAnswered").show();
 }
 
+$("input[type='radio']").on('change', function(value) {
+var question1 = $("input[name='q1']:checked").val();
+if (question1 === "true") {
+   correctAnswers++;
+   $("#correct").html("<h1>" + correctAnswers + "</h1>");
+} else if (question1 === "false") {
+   wrongAnswers++;
+   $("#wrong").html("<h1>" + wrongAnswers + "</h1>");
+}
+
+});
