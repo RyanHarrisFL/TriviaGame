818ba99976bdeb937ac961832fe3b7ecd82b791a
Rhino <ryanharrisfl@gmail.com>
2019-03-06 22:28:32 -0500

    update to update stop/start button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,16 @@ var unAnswered = 0;
 var intervalId;
 
 $("#start").on("click", startGame);
+$("#done").on("click", results);
+$("#countdown").html("<h1>" + countDown + "</h1>");
+
+$(document).ready(function(){
+    $("#questions").hide();
+    $("#final-results");
+    $("#start").click( function(){
+        $("section").show();
+    });
+  });
 
 function startGame() {
     clearInterval(intervalId);
@@ -15,7 +25,14 @@ function decrement() {
 countDown--;
 $("#countdown").html("<h1>" + countDown + "</h1>");
 
+if (countDown === 0) {
+    results();
 }
 
-function stop()
+}
+
+function results() {
+    clearInterval(intervalId);
+    console.log("You're a winner!")
+}
 
